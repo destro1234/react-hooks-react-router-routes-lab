4a874750f082bb51b3f5b6f2ff3ac65770a6d291
Elias <eliasventura23@gmail.com>
2022-09-09 11:00:15 -0400

    pushing code

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,32 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {
+        movies.map((m) => (
+          <div key={m.title}>
+            <h2>{m.title}</h2>
+            <p>{m.time}</p>
+            <ul>
+            {
+              m.genres.map( g => (
+                
+                  <li key={g}>{g}</li>
+                
+              ))
+            }
+            </ul>
+
+          </div>
+       
+        ))
+      }
+
+    </div>
+  )
 }
 
 export default Movies;
